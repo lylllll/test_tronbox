332a99ea9bc2be379dfb957973c5f8b62ccbe3f4
lylllll <73993249+lylllll@users.noreply.github.com>
2021-02-03 12:01:08 +0800

    Update 2_deploy_contracts.js

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var MyContract = artifacts.require("./IstanbulTest");
 var fk = artifacts.require("./addressNew");
 var mt = artifacts.require("./MetaCoin");
-var test0 = artifacts.require("./TestPayable");
+var test0 = artifacts.require("./TestFallback3");
 module.exports = function(deployer) {
    deployer.deploy(mt,10000);
    deployer.deploy(MyContract);
